cdcaf402745dc687ded3132c9dda69bde56a170e
ahmed <ahmed.soliman@edu.rptu.de>
2023-12-02 15:45:02 +0100

    test fails for creatvar, true, false

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -1,13 +1,22 @@
 #include "Manager.h"
 #include <iostream>
 
+
+using namespace std;
+
 using namespace ClassProject;
 
     Manager::Manager() {
-        std::cout << "Instantiated";
+        unique_table_attr false_row = {1,0,0,0,"False"};
+        unique_table_attr true_row = {2,0,0,0,"True"};
+        unique_table.push_back(false_row);
+        unique_table.push_back(true_row);
+        cout << "Constructor Instantiated" <<endl;
     }
 
     BDD_ID Manager::createVar(const std::string &label) {
-        BDD_ID test = 3;
-        return test;
     }
+    const BDD_ID&  Manager::False() {
+    }
+const BDD_ID & Manager::True(){
+}
--- src/test/Tests.cpp
+++ src/test/Tests.cpp
@@ -1,11 +1,27 @@
-//
-// Created by enano on 11/27/2023.
-//
 
 #include "Tests.h"
 
 using namespace ClassProject;
-TEST(Test, Test){
-    ClassProject::Manager FirstTest;
-    EXPECT_EQ(FirstTest.createVar("TestLabel"),2);
+
+TEST(Interfacefunctionstests, Managerinit_funcTest){
+    Manager constructorTest;
+    EXPECT_EQ(constructorTest.False(),1);
+    EXPECT_EQ(constructorTest.True(),2);
+}
+
+TEST(Interfacefunctionstests, createvar_functest ){
+    Manager createvarTest;
+    BDD_ID a = createvarTest.createVar("a");
+    EXPECT_EQ(a,3);
+
+}
+TEST(Interfacefunctionstests, false_functest ){
+    Manager false_test;
+    EXPECT_EQ(false_test.False(),1);
+
+}
+TEST(Interfacefunctionstests, true_functest ){
+    Manager true_test;
+    EXPECT_EQ(true_test.True(),2);
+
 }
