09823a395e37013a54971073556cd0175e44741a
root <root@DESKTOP-LL2UBK1>
2024-02-03 14:39:50 +0200

    part3 undef ref

--- src/reachability/Reachability.h
+++ src/reachability/Reachability.h
@@ -14,32 +14,13 @@ namespace ClassProject {
         std::vector<BDD_ID> transitionFunctions;
         std::vector<bool> initState;
     public:
-        //Reachability(unsigned int , unsigned int );
+        Reachability(unsigned int stateSize, unsigned int inputSize = 0);
 
         //~Reachability(unsigned int stateSize, unsigned int inputSize) = default;
 
         //const std::vector<BDD_ID> &getStates() override;
 
-        explicit Reachability(unsigned int stateSize, unsigned int inputSize = 0);
-
-        inline const std::vector<BDD_ID> &getStates() const override {
-            //return states;
-        }
-
-        inline const std::vector<BDD_ID> &getInputs() const override {
-            //return inputs;
-        }
-
-        bool isReachable(const std::vector<bool> &stateVector) override;
-        int stateDistance(const std::vector<bool> &stateVector) override;
-        void setTransitionFunctions(
-            const std::vector<BDD_ID> &transitionFunctions) override;
-        void setInitState(const std::vector<bool> &stateVector) override;
-
-        private:
-        BDD_ID existential_quantification(const BDD_ID &f,
-                                            const std::vector<BDD_ID> &v);
-            };
+       
 
 }
 #endif
